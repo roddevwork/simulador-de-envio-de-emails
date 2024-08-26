78bca9be399c35e9ef59827364576832634cca09
roddevwork <rodrigocardona87@gmail.com>
2024-08-25 21:19:05 -0300

    Add main.js with form handling and spinner functionality
    - Implement form handling logic in main.js
    - Add spinner styles in style.css
    - Improve general styling in style.css for better UI/UX

---
+++ main.js
@@ -0,0 +1,120 @@
+document.addEventListener('DOMContentLoaded', function () {
+	// Variables
+	const emailObj = {
+		email: '',
+		asunto: '',
+		mensaje: ''
+	}
+
+	// Seleccionar los elementos de la interfaz
+	const inputEmail = document.querySelector('#email')
+	const inputAsunto = document.querySelector('#asunto')
+	const inputMensaje = document.querySelector('#mensaje')
+	// console.log({ inputEmail, inputAsunto, inputMensaje })
+	const formulario = document.querySelector('#formulario')
+	const btnSubmit = document.querySelector(
+		'.button-group button[type="submit"]'
+	)
+	const btnReset = document.querySelector('.button-group button[type="reset"]')
+	const spinner = document.querySelector('#spinner')
+
+	// Asignar eventos
+	inputEmail.addEventListener('blur', validar)
+	inputAsunto.addEventListener('blur', validar)
+	inputMensaje.addEventListener('input', validar)
+	formulario.addEventListener('submit', enviarEmail)
+	btnReset.addEventListener('click', resetForm)
+
+	function validar(e) {
+		const campo = e.target.id.toUpperCase()
+		if (e.target.value.trim() === '') {
+			mostrarAlerta(`El campo ${campo} es obligatorio `, e.target.parentElement)
+			emailObj[e.target.name] = ''
+			comprobarEmail()
+			return
+		}
+		if (e.target.id === 'email' && !validarEmail(e.target.value)) {
+			mostrarAlerta('El email no es valido', e.target.parentElement)
+			emailObj[e.target.name] = ''
+			comprobarEmail()
+			return
+		}
+
+		limpiarAlerta(e.target.parentElement)
+
+		// Asignar los valores
+		emailObj[e.target.name] = e.target.value.trim().toLowerCase()
+
+		// Comprobar el objeto emailObj
+		comprobarEmail()
+	}
+
+	function mostrarAlerta(mensaje, referencia) {
+		limpiarAlerta(referencia)
+
+		// Generar alerta en html
+		const alerta_vacio_parrafo = document.createElement('P')
+		alerta_vacio_parrafo.textContent = mensaje
+		alerta_vacio_parrafo.classList.add('alerta-vacio')
+
+		// Inyectar el error al formulario
+		referencia.appendChild(alerta_vacio_parrafo)
+	}
+
+	function limpiarAlerta(referencia) {
+		// Comprueba si ya existe una alerta
+		const alerta = referencia.querySelector('.alerta-vacio')
+		if (alerta) {
+			alerta.remove()
+		}
+	}
+
+	function validarEmail(email) {
+		const regex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/
+		const resultado = regex.test(email)
+		return resultado
+	}
+
+	function comprobarEmail() {
+		// console.log(Object.values(emailObj).includes(''))
+
+		if (Object.values(emailObj).includes('')) {
+			btnSubmit.style.opacity = '0.5'
+			btnSubmit.style.cursor = 'not-allowed'
+			btnSubmit.disabled = true
+			return
+		}
+		btnSubmit.style.opacity = '1'
+		btnSubmit.style.cursor = 'pointer'
+		btnSubmit.disabled = false
+	}
+
+	function resetForm() {
+		emailObj.email = ''
+		emailObj.asunto = ''
+		emailObj.mensaje = ''
+		formulario.reset()
+		comprobarEmail()
+	}
+
+	function enviarEmail(e) {
+		e.preventDefault()
+		spinner.style.display = 'flex'
+
+		setTimeout(() => {
+			spinner.style.display = 'none'
+
+			resetForm()
+
+			// Crear una alerta
+			const alertaExito = document.createElement('P')
+			alertaExito.classList.add('alerta-exito-enviar')
+			alertaExito.textContent = 'Email enviado correctamente'
+			formulario.appendChild(alertaExito)
+
+			setTimeout(() => {
+				alertaExito.remove()
+			}, 3000)
+		}, 3000)
+	}
+})
